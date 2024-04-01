6d1e6f1e81f2d174db9edc95938dfb693f6a88c9
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-03-31 20:10:25 -0700

    Create Date class

---
+++ src/main/java/com/MMS/MMS/model/Date.java
@@ -0,0 +1,21 @@
+package com.MMS.MMS.model;
+
+public class Date {
+
+    // Static Variables
+
+    // Instance Variables
+    int dateDay;
+
+    // Constructors
+    public Date(int dateDay){
+        this.dateDay = dateDay;
+    }
+
+    // Methods
+    public int getDateDay(){
+        return dateDay;
+    };
+
+
+}
