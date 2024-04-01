6f1554f28bfb7c7b858eef9af9c24272e855d426
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-03-31 20:10:08 -0700

    Create Liability class

---
+++ src/main/java/com/MMS/MMS/model/Liability.java
@@ -0,0 +1,70 @@
+package com.MMS.MMS.model;
+
+import com.MMS.MMS.enums.ExpenseType;
+import com.MMS.MMS.enums.LiabilityType;
+import com.MMS.MMS.enums.PaymentStatus;
+
+import java.math.BigDecimal;
+
+public class Liability extends Expense implements Liabilities {
+
+    // Getters
+    public BigDecimal getTotalBalance(){
+        return totalBalance;
+    };
+    public LiabilityType getLiabilityType(){
+        return liabilityType;
+    };
+    public BigDecimal getInterestRate(){
+        return interestRate;
+    };
+
+    // Setters
+    public void setTotalBalance(BigDecimal totalBal){
+        this.totalBalance = totalBal;
+    };
+    public void setLiabilityType(LiabilityType liabType){
+        this.liabilityType = liabType;
+    };
+    public void setInterestRate(BigDecimal interest){
+        this.interestRate = interest;
+    };
+
+    // Constructor
+    public Liability (
+            String name,
+            BigDecimal charge,
+            int chargeFrequency,
+            Date dueDate,
+            Date startDate,
+            ExpenseType expenseType,
+            PaymentStatus paymentStatus,
+            BigDecimal lateFee,
+            int gracePeriod,
+            String notes,
+            BigDecimal totalBalance,
+            LiabilityType liabilityType,
+            BigDecimal interestRate
+    ){
+        super(  name,
+                charge,
+                chargeFrequency,
+                dueDate,
+                startDate,
+                expenseType,
+                paymentStatus,
+                lateFee,
+                gracePeriod,
+                notes);
+            this.totalBalance = totalBalance;
+            this.liabilityType = liabilityType;
+            this.interestRate = interestRate;
+
+    }
+
+    //Private Variables
+    private BigDecimal totalBalance;
+    private LiabilityType liabilityType;
+    private BigDecimal interestRate;
+
+}
