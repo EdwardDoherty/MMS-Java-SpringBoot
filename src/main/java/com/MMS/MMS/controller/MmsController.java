f5991f3b5b7121b9d15570a99440008bf77404aa
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-04-06 23:59:45 -0700

    Add login and dashboard controllers

--- src/main/java/com/MMS/MMS/controller/MmsController.java
+++ src/main/java/com/MMS/MMS/controller/MmsController.java
@@ -1,11 +1,16 @@
 package com.MMS.MMS.controller;
 
+import com.MMS.MMS.model.ErrorLoginMessage;
 import com.MMS.MMS.model.User;
 import com.MMS.MMS.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
 
 @Controller
 public class MmsController {
@@ -19,12 +24,14 @@ public class MmsController {
     @GetMapping(value = "/")
     public String index(Model model){
         model.addAttribute("expenseDB", model);
+
         return "homePage";
     }
 
     @GetMapping(value = "/thymeleafTemplate")
     public String getTemplate(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+
         return "thymeleafTemplate";
     }
 
@@ -34,6 +41,7 @@ public class MmsController {
     @GetMapping(value = "/createUser")
     public String createUser(Model model){
         model.addAttribute("newUser",new User());
+
         return "createUser";
     }
 
@@ -47,7 +55,6 @@ public class MmsController {
 
     @GetMapping("/getAllUsers")
     public String getUsers(Model model) {
-
         model.addAttribute("users", userRepository.findAll());
 
         return "getAllUsers";
@@ -56,11 +63,28 @@ public class MmsController {
     @PostMapping("/delete/{id}")
     public String deleteUser(@PathVariable String id) {
         userRepository.deleteById(id);
+
         return "redirect:/getAllUsers";
-
-
     }
 
+    @GetMapping("/login")
+    public String login(@RequestParam(name="error", required=false) String error, Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        model.addAttribute("error", error);
+        return "login";
+    }
+
+    @RequestMapping("/user/{id}")
+    public String loginAs(@PathVariable String id, HttpSession session) {
+        if(userRepository.existsById(id)) {
+            User userLogin = userRepository.findById(id).get();
+            session.setAttribute("loggedUser", userLogin);
+            return "dashboard";
+        }
+        else {
+            return "redirect:/login?error=true";
+        }
+    }
 
 
 }
