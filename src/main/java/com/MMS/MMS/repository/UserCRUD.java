2a6342368ec9d0e8af8981d6707feb736c8c8f7e
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-04-13 13:46:20 -0700

    Remove DAOs
    Looks like Spring Boot does DAOs for me. Too much magic happening behind the scenes....

--- src/main/java/com/MMS/MMS/repository/UserCRUD.java
+++ src/main/java/com/MMS/MMS/repository/UserCRUD.java
@@ -1,4 +1,16 @@
 package com.MMS.MMS.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserCRUD {
+
+    @Autowired
+    UserRepository userRepo;
+
+
+
+
+
 }
--- src/main/java/com/MMS/MMS/repository/UserRepository.java
+++ src/main/java/com/MMS/MMS/repository/UserRepository.java
@@ -1,16 +1,16 @@
 package com.MMS.MMS.repository;
 
-import com.MMS.MMS.dao.UserDAO;
 import com.MMS.MMS.model.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<UserDAO, String> {
+public interface UserRepository extends MongoRepository<User, String> {
 
     public Optional<User> findUserByUserName(String userName);
 
     public Optional<User> findUserByUserID(ObjectId userID);
 
+    public Optional<User> deleteUserByUserID(ObjectId userID);
 }
