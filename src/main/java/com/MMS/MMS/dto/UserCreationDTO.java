243534538ac5243422d06263a9aa48624ade69bb
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-04-14 00:40:43 -0700

    Update UserCreationDTO.java

--- src/main/java/com/MMS/MMS/dto/UserCreationDTO.java
+++ src/main/java/com/MMS/MMS/dto/UserCreationDTO.java
@@ -2,15 +2,17 @@ package com.MMS.MMS.dto;
 
 public class UserCreationDTO {
 
-    private String userName;
-    private String password;
+    private final String userName;
+//    private String password;
 
     public String getUserName() { return userName; }
 
-    public String getPassword() { return password; }
+//    public String getPassword() { return password; }
 
-    public UserCreationDTO(String userName, String password) {
+    // We aren't currently using the password but this will be here when I need it.
+//    public UserCreationDTO(String userName, String password) {
+    public UserCreationDTO(String userName) {
         this.userName = userName;
-        this.password = password;
+        //this.password = password;
     }
 }
