4d150ca1c20eece568a7d5d1d8d7991b2dea65b3
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-04-14 19:28:50 -0700

    Create Expense Controllers

---
+++ src/main/java/com/MMS/MMS/controllers/ExpenseGETController.java
@@ -0,0 +1,35 @@
+package com.MMS.MMS.controllers;
+
+import com.MMS.MMS.dto.ExpenseDTO;
+import com.MMS.MMS.dto.ExpenseListDTO;
+import com.MMS.MMS.model.Expense;
+import com.MMS.MMS.repository.ExpenseRepository;
+import com.MMS.MMS.service.mappers.ExpenseDTOMapper;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class ExpenseGETController {
+
+    @Autowired
+    private ExpenseRepository expenseRepository;
+
+    @GetMapping("/getExpenseList")
+    @ResponseBody
+    public ExpenseListDTO getExpenseDTOsByUserID(ObjectId userID) {
+        List<Expense> expenses = expenseRepository.findAllByUserID(userID);
+
+        List<ExpenseDTO> expenseDTOList = new ArrayList<ExpenseDTO>();
+        // This is stupid but it works for now. I'm converting it back and forth but whatever
+        expenses.forEach((expense) -> expenseDTOList.add(ExpenseDTOMapper.toDTO(expense)) );
+
+        return new ExpenseListDTO(expenseDTOList);
+    }
+
+}
--- src/main/java/com/MMS/MMS/controllers/ExpensePOSTController.java
+++ src/main/java/com/MMS/MMS/controllers/ExpensePOSTController.java
@@ -2,23 +2,17 @@ package com.MMS.MMS.controllers;
 
 import com.MMS.MMS.dto.UserDTO;
 import com.MMS.MMS.dto.ExpenseDTO;
-import com.MMS.MMS.dto.ExpenseListDTO;
-import com.MMS.MMS.model.Expense;
 import com.MMS.MMS.model.User;
 import com.MMS.MMS.repository.ExpenseRepository;
-import com.MMS.MMS.service.mappers.ExpenseDTOMapper;
 import jakarta.servlet.http.HttpSession;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
-public class ExpenseController {
+public class ExpensePOSTController {
 
     @Autowired
     private ExpenseRepository expenseRepository;
@@ -55,17 +49,7 @@ public class ExpenseController {
         return "redirect:/viewExpenses";
     }
 
-    @GetMapping("/getExpenseList")
-    @ResponseBody
-    public ExpenseListDTO getExpenseDTOsByUserID(ObjectId userID) {
-        List<Expense> expenses = expenseRepository.findAllByUserID(userID);
 
-        List<ExpenseDTO> expenseDTOList = new ArrayList<ExpenseDTO>();
-        // This is stupid but it works for now. I'm converting it back and forth but whatever
-        expenses.forEach((expense) -> expenseDTOList.add(ExpenseDTOMapper.toDTO(expense)) );
-
-        return new ExpenseListDTO(expenseDTOList);
-    }
 
 
 }
