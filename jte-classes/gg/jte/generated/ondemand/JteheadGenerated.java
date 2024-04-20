65f940b11c9fcde3f911634f5352c79513f3f7dc
Edward Doherty <109316023+EdwardDoherty@users.noreply.github.com>
2024-04-20 00:48:38 -0700

    Add CSS to head.jte

--- jte-classes/gg/jte/generated/ondemand/JteheadGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JteheadGenerated.java
@@ -7,7 +7,7 @@ public final class JteheadGenerated {
 		jteOutput.writeContent("\r\n<head>\r\n    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />\r\n    <title>");
 		jteOutput.setContext("title", null);
 		jteOutput.writeUserContent(title);
-		jteOutput.writeContent("</title>\r\n    <link href=\"/static/css/main.css\" rel=\"stylesheet\" />\r\n    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\r\n    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\r\n    <link href=\"https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap\" rel=\"stylesheet\">\r\n    <script src=\"https://kit.fontawesome.com/86a01f560a.js\" crossorigin=\"anonymous\"></script>\r\n</head>");
+		jteOutput.writeContent("</title>\r\n    <link href=\"/css/main.css\" rel=\"stylesheet\" />\r\n    <link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\r\n    <link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\r\n    <link href=\"https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap\" rel=\"stylesheet\">\r\n    <script src=\"https://kit.fontawesome.com/86a01f560a.js\" crossorigin=\"anonymous\"></script>\r\n</head>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		String title = (String)params.getOrDefault("title", "MMS - App");
